7b344a2332ad2b028053ee25b62384c5320aae33
Jignesh Patel <9671042+Jignesh-UI@users.noreply.github.com>
2018-12-27 16:20:43 +0530

    Add files via upload

---
+++ src/app/common/left-profile/left-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-profile',
+  templateUrl: './left-profile.component.html',
+  styleUrls: ['./left-profile.component.scss']
+})
+export class LeftProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
